0e26bd28250beed2fe8048927adf5d3c2fd5d679
Vamsi <pusarlatarunvamsi@gmail.com>
2023-12-20 13:42:48 +0530

    updated

---
+++ script.js
@@ -0,0 +1,61 @@
+function addComponent() {
+    var componentsTableBody = document.querySelector("#componentsTable tbody");
+
+    // Check if required fields are filled
+    if (validateInputs()) {
+        var newRow = componentsTableBody.insertRow();
+        newRow.innerHTML = `
+            <td><input type="text" class="subject" placeholder="Enter Component"></td>
+            <td><input type="number" class="marks" placeholder="Marks obtained" required></td>
+            <td><input type="number" class="maxMarks" placeholder="Max. Marks" required></td>
+            <td><input type="number" class="weightage" placeholder="Weighate" required></td>
+            <td><button class="deleteButton" onclick="deleteRow(this)">Delete</button></td>
+        `;
+    }
+}
+
+function deleteRow(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function calculateInternal() {
+    // Check if required fields are filled
+    if (validateInputs()) {
+        var totalMarks = 0;
+        var rows = document.querySelectorAll("#componentsTable tbody tr");
+
+        rows.forEach(function (row) {
+            var marks = parseFloat(row.querySelector(".marks").value) || 0;
+            var maxMarks = parseFloat(row.querySelector(".maxMarks").value) || 1;
+            var weightage = parseFloat(row.querySelector(".weightage").value) || 0;
+
+            // Check if Marks Obtained is greater than Maximum Marks
+            if (marks > maxMarks) {
+                alert("Marks Obtained cannot be greater than Maximum Marks.");
+                return;
+            }
+
+            totalMarks += (marks / maxMarks) * weightage;
+        });
+
+        document.getElementById("totalMarks").innerText = totalMarks.toFixed(2);
+    }
+}
+
+function validateInputs() {
+    var rows = document.querySelectorAll("#componentsTable tbody tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var marks = rows[i].querySelector(".marks").value.trim();
+        var maxMarks = rows[i].querySelector(".maxMarks").value.trim();
+        var weightage = rows[i].querySelector(".weightage").value.trim();
+
+        if (marks === "" || maxMarks === "" || weightage === "") {
+            alert("Please fill in all required fields.");
+            return false;
+        }
+    }
+
+    return true;
+}
